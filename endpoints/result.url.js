138a58932437bb1b455f12fb315cd69ab3d4727b
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2020-06-08 20:59:31 +0300

    #fix body-parser positioning

--- endpoints/result.url.js
+++ endpoints/result.url.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 router.post('/', (req, res) => {
-    fs.appendFile(path.join('', 'result.txt'), JSON.stringify(req.body), (err) => {
+    console.log(req.body);
+    fs.appendFile(path.join('./endpoints/', 'result.txt'), JSON.stringify(req.body).concat('\n'), (err) => {
         if (err)
             console.error(err);
         console.log('Data written to file...');
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./endpoints/index');
-app.use('/craft', routes);
 app.use(bodyParser.json({urlencoded: true}));
+app.use('/craft', routes);
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server listening on port ${server.address().port}`);
 });
